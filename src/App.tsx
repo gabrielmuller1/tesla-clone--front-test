34a0d09a15b9a6f432f541ed79a08b3ef505a195
Gabriel Cezar Muller <81782103+gabrielmuller1@users.noreply.github.com>
2022-02-21 13:35:44 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Page from './components/Page'
+
+import { GlobalStyle } from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <Page />
+
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App;
